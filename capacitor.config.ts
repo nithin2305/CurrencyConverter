c1550c270d17f560ec5495f9ee8869cdc2746d0b
nithin2305 <nithinmishra99@gmail.com>
2025-12-27 09:29:24 +0530

    feat: update dependencies and add Capacitor support
    - Added Capacitor dependencies to package.json for Android support.
    - Updated assetlinks.json with the correct SHA256 certificate fingerprint.
    - Enhanced styling in currency-converter.component.css for better dark mode support and responsiveness.
    - Updated TWA manifest with new app version and code.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'currency-converter',
-  "webDir": "dist/currency-converter",
+  appId: 'com.example.currency',
+  appName: 'CurrencyConverter',
+  webDir: 'dist/currency-converter',
+  bundledWebRuntime: false,
+  server: {
+    url: 'https://currencyconverternithin.netlify.app',
+    cleartext: false
+  }
 };
 
 export default config;
